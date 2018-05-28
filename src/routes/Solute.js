69e83a0837f3cdedbda35b273a3fc5ef8324b60e
13025214712 <13025214712@163.com>
2018-05-28 14:31:44 +0800

    work

--- src/routes/Solute.js
+++ src/routes/Solute.js
@@ -6,8 +6,183 @@ class Solute extends React.Component {
   render() {
     return (
       <div>
-        <img src={require('../assets/solute1.jpg')} style={{ width: '100%', minWidth: '800px' }} alt="" />
-        <img src={require('../assets/solute2.jpg')} style={{ width: '100%', minWidth: '800px' }} alt="" />
+        <img src={require('../assets/solute1.jpg')} style={{ width: '100%' }} alt="" />
+
+        <div className={styles.two}>
+          <div className={styles.section}>
+            <div className={styles.section1}>
+              <img src={require('../assets/solute/head1.png')} alt="" />
+              <div className={styles.h2}>——<span>企业会员精准营销顾问咨询</span>——</div>
+            </div>
+            <div className={styles.section1}>
+              <img src={require('../assets/solute/head2.png')} alt="" />
+              <div className={styles.h2}>——<span>汇员帮-会员精准营销系统</span>——</div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute1.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>帮助企业全面了解目前会员状况</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>进行会员的数据清洗，测算分析实际会员数量，并区分有效会员和无效会员</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function1.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>管理会员</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>会员资料随时查阅，方便会员使用和门店管理</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute2.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>帮助企业进行客户画像</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>通过画像找出共性和个性的客户特点，并根据画像制定有针对性的会员营销策略</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function2.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>分析会员</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>为会员行为打标签，优化会员结构，自动分类分级</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute3.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>帮助企业优化或重新搭建会员体系</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>包括会员等级的划分、会员权益的设置，并协助落地实施</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function3.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>电子会员卡</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>电子会员卡取代实体卡，提升效率和客户体验</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute4.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>帮助企业策划会员精准营销活动</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>对会员营销活动人群、产品及方式提出专业分析，以提高活动效果</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function4.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>精准营销</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>通过会员营销模型，精准推送回访名单、商品和话术</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute5.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>针对会员运营分析报告，协助制定改进方案</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>跟行业数据进行对比分析，召集相关部门/人员共同研究解决方案并协助执行</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function5.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>业绩管理</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>随时随地了解门店销售和员工业绩</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute6.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>帮助企业完善和改进微信公众号运营</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>通过分析公众号运营数据，提出栏目、内容及活动等改进建议</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function6.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>灵活拓客</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>通过新增会员功能，随时开拓新客户</div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.section}>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/solute/solute7.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>为企业提供有针对性的培训</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>包含会员营销知识理论、汇员帮系统操作、门店会员管理及服务等相关培训内容</div>
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <div className={styles.section22}>
+                <img src={require('../assets/home/function7.png')} alt="" />
+              </div>
+              <div className={styles.section21}>
+                <div className={styles.text1}>学习提升</div>
+                <img src={require('../assets/home/line.png')} alt="" />
+                <div className={styles.text2}>随时随地学习商学院母婴知识及销售管理课程</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
         <div className={styles.afterSale}>
           <div className={styles.title}>—— <span>售后服务</span> ——</div>
 
